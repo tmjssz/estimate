25701b31ddb5462405cc2e805f59737f05c23f93
Tim-Jonas Schwarz <tim@timjonasmacbook.fritz.box>
2014-04-22 11:03:52 +0200

    Deploy script angepasst

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,17 @@
+#!/bin/sh
+FILE=../estimate.db
+
 # Virutelle Python-Umgebung mit django aktivieren
 . ~/.virtualenvs/django-workshop/bin/activate
 
-# Daten exportieren
-python manage.py dumpdata --indent 4 questions > questions/fixtures/initial_data.json
-python manage.py dumpdata --indent 4 auth.User > userauth/fixtures/initial_data.json
+# Prüfen, ob Datenbank schon existiert
+if [ -e $FILE ];
+then
+	echo "Export data from database: $FILE"
+	# Daten exportieren
+	python manage.py dumpdata --indent 4 questions > questions/fixtures/initial_data.json
+	python manage.py dumpdata --indent 4 auth.User > userauth/fixtures/initial_data.json
+fi
 
 # SQL Queries von den Models ausführen
 python manage.py syncdb
