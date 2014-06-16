d98e5908709746b337ae0e092329100edc8ed5e7
Tim-Jonas Schwarz <tim@timjonasmacbook.fritz.box>
2014-06-16 22:44:47 +0200

    Anpassung für deployment

--- estimate/wsgi.py
+++ estimate/wsgi.py
@@ -8,6 +8,11 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+sys.path.insert(0, '/srv/www')
+sys.path.insert(0, '/srv/www/estimate-master')
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "estimate.settings")
 
 from django.core.wsgi import get_wsgi_application
