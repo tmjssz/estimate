33697e1307910f80389f87d73e091e795b21a4dc
Tim-Jonas Schwarz <tim@timjonasmacbook.fritz.box>
2014-06-20 15:25:26 +0200

    Email Versand an Admin bei User Registrierung

--- userauth/views.py
+++ userauth/views.py
@@ -4,6 +4,10 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.contrib.auth import authenticate, login
+from django.core.mail import mail_admins
+from django.template import Context
+from django.template.loader import get_template
+from django.conf import settings
 import logging
 
 logger = logging.getLogger('estimate.userauth.views')
@@ -20,6 +24,12 @@ def register(request, template_name='userauth/register.html', next_page_name=Non
             pwd = request.POST[u'password1']
             new_user = authenticate(username=username, password=pwd)
             login(request, new_user)
+
+            template = get_template('userauth/mail-user-registered.html')
+            context = Context({'user': new_user, 'host': settings.EMAIL_HTML_CONTENT_HOST})
+            content = template.render(context)
+            mail_admins('[Neuer User] '+ new_user.username, 'Es hat sich ein neuer User namens '+new_user.username+' registriert.', html_message=content, fail_silently=True)
+
             if next_page_name is None:
                 next_page = '/'
             else:
