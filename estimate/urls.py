f8b500eaf47fc17e53ce69ed0616862c2ec9f216
Tim-Jonas Schwarz <tim@wlan-141-23-99-216.tubit.tu-berlin.de>
2014-06-30 12:34:34 +0200

    Footer & Feedback Form

--- estimate/urls.py
+++ estimate/urls.py
@@ -11,4 +11,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('questions.urls')),
     url(r'^', include('userauth.urls')),
+    (r'^kontakt/', include('contact_form.urls')),
 )
