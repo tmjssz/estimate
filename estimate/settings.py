51cc91af67db948ce6e382baf38f6a23d8cec06f
Tim-Jonas Schwarz <tim@timjonasmacbook.fritz.box>
2014-09-12 22:54:58 +0200

    Google Social Login wieder rein genommen

--- estimate/settings.py
+++ estimate/settings.py
@@ -76,9 +76,9 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 )
 
 AUTHENTICATION_BACKENDS = (
-   'social.backends.facebook.FacebookOAuth2',
+   #'social.backends.facebook.FacebookOAuth2',
    'social.backends.google.GoogleOAuth2',
-   'social.backends.twitter.TwitterOAuth',
+   #'social.backends.twitter.TwitterOAuth',
    'django.contrib.auth.backends.ModelBackend',
 )
 
